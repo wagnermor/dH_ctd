40cff9fb38b0a1dcdf68282fcf79d89cecc12cbe
WagnerMor <wagnermor@outlook.com>
2021-09-08 17:21:12 -0300

    fix: checkPoint001 project

--- programacaoImperativa/aula012/exe.js
+++ programacaoImperativa/aula012/exe.js
@@ -1,23 +1,33 @@
-const alicia = [23, 69, 32];
-const bob = [12, 67, 43];
+// const alicia = [23, 69, 32];
+// const bob = [12, 67, 43];
 
-function encontrarGanhador(a, b) {
-  let pontosAlicia = 0;
-  let pontosBob = 0;
+// function encontrarGanhador(a, nomeDeA, b, nomeDeB) {
+//   let pontosAlicia = 0;
+//   let pontosBob = 0;
   
-  if (alicia.length === bob.length) {
+//   if (alicia.length === bob.length) {
     
-    for (let i = 0; i < alicia.length; i += 1 ) { 
+//     for (let i = 0; i < alicia.length; i += 1 ) { 
       
-      if (a[i] > b[i]) {
-        pontosAlicia++;
-      } else if(a[i] < b[i]) {
-        pontosBob += 1;
-      } 
-    }
+//       if (a[i] > b[i]) {
+//         pontosAlicia++;
+//       } else if(a[i] < b[i]) {
+//         pontosBob += 1;
+//       } 
+//     }
+//   }
+
+//   return (pontosAlicia > pontosBob) ? `Alicia com ${pontosAlicia} pontos` : `Bob com ${pontosBob}`;
+// }
+
+// console.log(`O ganhador é: ${encontrarGanhador(alicia, bob)}`);
+let n = [1,2,3]
+
+function somaArray(a){
+  let soma = 0;
+  for (let i = 0; i < a.length; i++) {
+    soma += a[i];
   }
-
-  return (pontosAlicia > pontosBob) ? `Alicia com ${pontosAlicia} pontos` : `Bob com ${pontosBob}`;
-}
-
-console.log(`O ganhador é: ${encontrarGanhador(alicia, bob)}`);
+      return soma;
+ }
+ console.log(somaArray(n));
